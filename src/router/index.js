ba147e782f6d79864b6a4d26fa4eec08e7d1f048
hinyarigohan <hinyarigohan@gmail.com>
2021-09-14 17:31:47 +0900

    Google Analytics を実装

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Top from '@/pages/Top'
+import VueGtag from 'vue-gtag'
 
 import Syllabus from '@/pages/Syllabus'
 import Institution from '@/pages/Institution'
 
-Vue.use(Router)
+if (process.env.NODE_ENV === "production") {
+    Vue.use(VueGtag, {
+        config: {id: process.env.VUE_APP_GA_ID}
+    },Router)
+} else {
+    Vue.use(Router)
+}
+
 
 export default new Router({
     mode: 'history',
